ba670d713c5113963d45e14859c84d7abd1aee6f
rohanbari <rohanbari4@gmail.com>
2022-06-30 15:01:56 +0530

    Learning the basics of strings

---
+++ src/string_basics.py
@@ -0,0 +1,25 @@
+# All about strings goes here.
+
+# Printing a huge string
+big_string = """Hello, everyone! I am Rohan B.,
+from Kolkata, India. I have a great enthusiasm
+towards Computer Science."""
+print(big_string)
+
+# Strings are arrays
+str_first = "Hello, world!"
+print(str_first[4]) # Prints the 'o' letter
+
+# Looping through a string
+for elem in "banana":
+    print(elem)
+
+# String length
+print(len(str_first))
+
+# Checking if 'India' exists in big_string
+print('India' in big_string)
+# Note: 'not in' can be used to check whether something NOT exists.
+
+
+
