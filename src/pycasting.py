825321833ae27f221815d5b2749afa623b826a75
rohanbari <rohanbari4@gmail.com>
2022-06-30 14:36:47 +0530

    Having a look at type-casting

---
+++ src/pycasting.py
@@ -0,0 +1,7 @@
+# Type casting in Python
+
+number_in_str = "4096"
+pure_num = int(number_in_str);
+
+print("Number in string: " + number_in_str)
+print("Type-casted number: " + str(type(pure_num)) + str(pure_num))
