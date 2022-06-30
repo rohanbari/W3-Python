584c8141cc4550eb2ecf5e139b0f2aa4d368116b
rohanbari <rohanbari4@gmail.com>
2022-06-30 13:15:24 +0530

    Built-in data types in Python

---
+++ src/data_types.py
@@ -0,0 +1,34 @@
+# Built-in Data Types in Python
+#
+# Text type:      str
+# Numeric types:  int, float, complex
+# Sequence types: list, tuple, range
+# Mapping type:   dict
+# Set types:      set, frozenset
+# Boolean:        bool
+# Binary types:   bytes, bytearray, memoryview
+# None type:      NoneType
+#
+
+string_var = "Python is becoming more interesting now!"
+
+int_var = 1024
+float_var = 32766.75
+complex_var = 3 + 4j
+
+list_var = ['apple', 'ball', 'cat']
+tuple_var = ('balance', 475, 11.50, 'no_insurance')
+range_var = range(10)
+
+dict_var = {"first_name" : 'John', "last_name" : 'Wick', "age" : 33}
+
+set_var = {'apple', 'banana', 'cherry', 'dates'}
+frozenset_var = frozenset({'apple', 'banana', 'cherry'})
+
+boolean_var = True
+
+bytes_var = b"This is a byte type"
+bytearray_var = bytearray(5)
+memoryview_var = memoryview(bytes(5))
+
+nonetype_var = None
