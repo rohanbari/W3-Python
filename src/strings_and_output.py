967b94af5e9e0fbca29a52bea3d2463c0b6f5c95
rohanbari <rohanbari4@gmail.com>
2022-06-30 12:25:46 +0530

    Output variables and strings

---
+++ src/strings_and_output.py
@@ -0,0 +1,12 @@
+# Using strings and outputting them.
+
+my_first_string = "Python is awesome!"
+print(my_first_string)
+
+# Unpacking the list, assigning variables, and printing out them
+
+list_words = ['Python', 'is', 'easy']
+first, second, third = list_words
+
+print(first + ' ' + second + ' ' + third)
+print(list_words) # Printing the list object
