0023460c01101ca11d8e73adda3497c006991fc2
rohanbari <rohanbari4@gmail.com>
2022-06-30 14:07:27 +0530

    Playing with numbers and type()

---
+++ src/number_examples.py
@@ -0,0 +1,15 @@
+# Various examples related to Python Numbers
+
+some_number = 420120
+my_imaginary_salary = 500000.25
+complex_expr = 3 + 4j
+
+# Printing the values of variables.
+print(some_number)
+print(my_imaginary_salary)
+print(complex_expr)
+
+# Printing the types of the variables.
+print(type(some_number))
+print(type(my_imaginary_salary))
+print(type(complex_expr))
