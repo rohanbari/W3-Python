2cb57882ce46d613ce993d38add1947ead184112
rohanbari <rohanbari4@gmail.com>
2022-07-01 09:06:53 +0530

    Learned how to concatenate strings

---
+++ src/string_concatenation.py
@@ -0,0 +1,7 @@
+# A bunch of strings are useful, but sometimes, they need to be summed up.
+
+first_str = "Hello, "
+second_str = "world!"
+
+# The '+' operator concatenates both strings.
+print(first_str + second_str)
