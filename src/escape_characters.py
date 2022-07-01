e1a290f585f04b6da0a6d412129cea4eb7e930fa
rohanbari <rohanbari4@gmail.com>
2022-07-01 09:38:57 +0530

    Escape sequences/characters in Python

---
+++ src/escape_characters.py
@@ -0,0 +1,12 @@
+# What happens when a string is defined like: "Hello, I live in "Kolkata""?
+# We might understand it, but not the Python interpreter.
+
+# Comment out to see the error.
+# sample_str = "The Santa Claus lives in "North Pole""
+
+# The correct approach is:
+sample_str = "The Santa Claus lives in \"North Pole\""
+print(sample_str)
+
+print('Refer to the link below to know more of escape chars.')
+print('https://www.w3schools.com/python/python_strings_escape.asp')
