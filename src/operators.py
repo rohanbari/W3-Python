5ec40376fe33921755af007bd8b4bc8a51b4c5d3
rohanbari <rohanbari4@gmail.com>
2022-07-02 07:45:55 +0530

    Learning the operators

---
+++ src/operators.py
@@ -0,0 +1,17 @@
+# How would we solve mathematical expressions in programming? Well, that is why
+# operators exist. Let's have a look at them.
+ 
+str = """Python supports these operators:
+  op1 [] op2, [] can be +, -, *, /, %, ** (to the power), // (floor div.)
+Like any other programming language, it supports assignment operators, too.
+  op [] val, [] can be =, +=, -=, *=, /=, %=, //=, **=, &=, |=, ^=, >>=, <<=
+Comparison operators are the exact same as in any other language.
+  op1 [] op2, [] can be ==, !=, >, <, >=, <=
+Logical operators are no exception.
+  op1 [] op2, [] can be and, or, not, (is, is not -- only for objects)
+Membership operators:
+  obj1 [] obj2, [] can be in, not in
+Bitwise operators are used to compare binary numbers:
+  op1 [] op2, [] can be &, |, ^, ~, <<, >>""";
+
+print(str)
